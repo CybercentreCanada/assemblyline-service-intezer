68ba15d29f9c261316eb2f6eb1de1a9f601125b5
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2022-08-09 14:06:52 +0000

    Adding new heur for families of interest, creating safelist of families

---
+++ safe_families.py
@@ -0,0 +1,37 @@
+# This is a typed list of families that we will not be flagging as a family of interest
+SAFE_FAMILIES = {
+    "administration_tool": [
+        "RealVNC",
+        "FileZilla Project",
+        "TightVNC",
+        "Sysinternals",
+        "tftp",
+        "WinDirStat",
+        "Wireshark",
+        "UltraVNC",
+        "NetSupport Ltd",
+        "Mobatek",
+        "Devolutions inc.",
+        "PuTTY",
+        "TeamViewer GmbH",
+        "HashiCorp",
+        "traceroute",
+        "WinSCP",
+        "OpenSSH",
+        "AnyDesk Software GmbH",
+        "Wget",
+    ],
+    "installer": [
+        "InstallShield",
+        "NSIS",
+        "InnoSetup",
+        "WEXTRACT",
+        "InstallAnywhere",
+        "Advanced",
+        "7-Zip.SFX",
+        "WinRAR.SFX",
+    ],
+    "packer": [
+        "UPX",
+    ]
+}
